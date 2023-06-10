2ea35a0d818e132fd02dd67d2d61477ad3ccfd6f
David Miguel Susano Pinto <pinto@robots.ox.ac.uk>
2023-06-10 01:09:34 +0100

    follow_things_around: apply black code formatting.

--- follow_things_around/__init__.py
+++ follow_things_around/__init__.py
@@ -42,37 +42,43 @@ _logger = logging.getLogger()
 
 USE_GPU: bool = True
 
-DEFAULT_DEVICE: str = 'cuda'
+DEFAULT_DEVICE: str = "cuda"
 
 UNKNOWN_TRACK_ID_MARKER: int = -1
 
-FFMPEG_LOG_LEVEL: str = 'info'
+FFMPEG_LOG_LEVEL: str = "info"
 
 MATCH_OVERLAP_THRESHOLD: float = 0.6
 
-VIDEO_FILE: str = ''
+VIDEO_FILE: str = ""
 
 
 def subprocess_print_stderr(args: List[str]) -> None:
     p = subprocess.Popen(args, text=True, stderr=subprocess.PIPE)
 
-    for line in iter(p.stderr.readline, ''):
-        print(line, end='')
+    for line in iter(p.stderr.readline, ""):
+        print(line, end="")
     p.wait()
     if p.returncode != 0:
-        raise Exception('subprocess failed')
+        raise Exception("subprocess failed")
 
 
 def ffmpeg_video_to_frames(video_fpath: str, frames_dir: str) -> None:
     subprocess_print_stderr(
         [
-            'ffmpeg',
-            '-i', video_fpath,
-            '-vsync', 'vfr',
-            '-q:v', '1',
-            '-start_number', '0',
-            '-filter:v', 'scale=iw:ih*(1/sar)',
-            '-loglevel', FFMPEG_LOG_LEVEL,
+            "ffmpeg",
+            "-i",
+            video_fpath,
+            "-vsync",
+            "vfr",
+            "-q:v",
+            "1",
+            "-start_number",
+            "0",
+            "-filter:v",
+            "scale=iw:ih*(1/sar)",
+            "-loglevel",
+            FFMPEG_LOG_LEVEL,
             # FIXME: what if %06d.jpg is not enough and rools over?
             os.path.join(FRAMES_DIR, "%06d.jpg"),
         ]
@@ -82,11 +88,15 @@ def ffmpeg_video_to_frames(video_fpath: str, frames_dir: str) -> None:
 def ffprobe_get_frame_rate(video_fpath: str) -> float:
     ffprobe_p = subprocess.run(
         [
-            'ffprobe',
-            '-loglevel', 'panic',
-            '-select_streams', 'v',
-            '-show_entries', 'stream=r_frame_rate',
-            '-print_format', 'csv',
+            "ffprobe",
+            "-loglevel",
+            "panic",
+            "-select_streams",
+            "v",
+            "-show_entries",
+            "stream=r_frame_rate",
+            "-print_format",
+            "csv",
             video_fpath,
         ],
         check=True,
@@ -94,7 +104,7 @@ def ffprobe_get_frame_rate(video_fpath: str) -> float:
         text=True,
     )
     # we expect something like "stream,25/1\n"
-    frame_rate_parts = ffprobe_p.stdout.split(',')[1][:-1].split('/')
+    frame_rate_parts = ffprobe_p.stdout.split(",")[1][:-1].split("/")
     if len(frame_rate_parts) == 1:
         return float(frame_rate_parts[0])
     else:  # format is something such as 25/1
@@ -107,18 +117,28 @@ def ffmpeg_video_from_frames_and_video(
     frame_rate = ffprobe_get_frame_rate(in_video_fpath)
     subprocess_print_stderr(
         [
-            'ffmpeg',
-            '-y',  # overwrite output files without asking
-            '-loglevel', FFMPEG_LOG_LEVEL,
-            '-framerate', str(frame_rate),
-            '-pattern_type', 'glob',
-            '-i', os.path.join(frames_dir, '*.jpg'),
-            '-i', in_video_fpath,
-            '-c:a', 'aac',  # https://github.com/ox-vgg/chimpanzee-tracking/issues/1
-            '-c:v', 'libx264',
-            '-map', '0:v:0',  # use video from input 0 / stream 0
-            '-map', '1:a:0',  # use audio from input 1 / stream 0
-            '-pix_fmt', 'yuv420p',
+            "ffmpeg",
+            "-y",  # overwrite output files without asking
+            "-loglevel",
+            FFMPEG_LOG_LEVEL,
+            "-framerate",
+            str(frame_rate),
+            "-pattern_type",
+            "glob",
+            "-i",
+            os.path.join(frames_dir, "*.jpg"),
+            "-i",
+            in_video_fpath,
+            "-c:a",
+            "aac",  # https://github.com/ox-vgg/chimpanzee-tracking/issues/1
+            "-c:v",
+            "libx264",
+            "-map",
+            "0:v:0",  # use video from input 0 / stream 0
+            "-map",
+            "1:a:0",  # use audio from input 1 / stream 0
+            "-pix_fmt",
+            "yuv420p",
             out_video_fpath,
         ]
     )
@@ -147,7 +167,7 @@ def detect(
 ):
     if visual_threshold < 0.0 or visual_threshold > 1.0:
         raise ValueError(
-            'visual_threshold needs to be a number between 0.0 and 1.0'
+            "visual_threshold needs to be a number between 0.0 and 1.0"
         )
 
     d2_cfg = detectron2.config.get_cfg()
@@ -156,9 +176,9 @@ def detect(
     d2_cfg.MODEL.DEVICE = DEFAULT_DEVICE
 
     predictor = detectron2.engine.DefaultPredictor(d2_cfg)
-    _logger.info('Finished loading model %s', detection_model_config_path)
+    _logger.info("Finished loading model %s", detection_model_config_path)
 
-    _logger.info('Starting detection phase')
+    _logger.info("Starting detection phase")
 
     frame_id_to_filename: Dict[str, str] = {}
     frame_detections: Dict[str, Dict[str, Detection]] = defaultdict(dict)
@@ -170,7 +190,7 @@ def detect(
 
         if frame_index % 1000 == 0:
             _logger.info(
-                'Starting to process images %d to %d',
+                "Starting to process images %d to %d",
                 frame_index,
                 min(frame_index + 1000 - 1, len(video_frames) - 1),
             )
@@ -178,7 +198,9 @@ def detect(
         # Acquire image
         img = cv2.imread(frame_fpath)
         outputs = predictor(img)
-        bboxes = outputs['instances'].pred_boxes[outputs['instances'].pred_classes == class_idx]
+        bboxes = outputs["instances"].pred_boxes[
+            outputs["instances"].pred_classes == class_idx
+        ]
 
         for i, bbox in enumerate(bboxes):
             frame_detections[frame_id][str(i)] = Detection(
@@ -189,7 +211,7 @@ def detect(
                 h=float(bbox[3] - bbox[1]),
             )
 
-    _logger.info('Finished detections')
+    _logger.info("Finished detections")
     return frame_detections, frame_id_to_filename
 
 
@@ -198,25 +220,25 @@ def track(
     frame_id_to_filename: Dict[str, str],
     tracking_model_path: str,
 ):
-    _logger.info('Starting tracking phase')
+    _logger.info("Starting tracking phase")
 
     tracker_config = {
-        'gpu_id': 0 if USE_GPU else -1,
-        'verbose': True,
-        'preload_model': True,
+        "gpu_id": 0 if USE_GPU else -1,
+        "verbose": True,
+        "preload_model": True,
     }
 
     detections_match_config = {
-        'match_overlap_threshold': MATCH_OVERLAP_THRESHOLD,
-        'UNKNOWN_TRACK_ID_MARKER': UNKNOWN_TRACK_ID_MARKER,
-        'frame_img_dir': '',
-        'verbose': True,
-        'via_project_name': VIDEO_FILE,
+        "match_overlap_threshold": MATCH_OVERLAP_THRESHOLD,
+        "UNKNOWN_TRACK_ID_MARKER": UNKNOWN_TRACK_ID_MARKER,
+        "frame_img_dir": "",
+        "verbose": True,
+        "via_project_name": VIDEO_FILE,
     }
 
     # We expect videos to be of a single shot, hence only shot_id 0.
     # XXX: Why are we using strings for int keys?
-    shot_id = '0'
+    shot_id = "0"
     detections4svt = {shot_id: {x: {} for x in frame_id_to_filename.keys()}}
     for frame_id, detections_values in detections.items():
         for box_id, detection in detections_values.items():
@@ -236,7 +258,7 @@ def track(
         svt_detections.match(tracker=tracker, config=detections_match_config)
 
     _logger.info(svt_stdout.getvalue())
-    _logger.info('Finished tracking')
+    _logger.info("Finished tracking")
     return svt_detections
 
 
@@ -255,7 +277,7 @@ def display_detections(
         min=0,
         max=len(frame_id_filename_pair) - 1,
         step=1,
-        orientation='horizontal',
+        orientation="horizontal",
         # description and readout are disabled because we'll show the
         # frame filename in the label ourselves.
         description="",
@@ -264,14 +286,14 @@ def display_detections(
         continuous_update=False,
     )
     previous_button = ipywidgets.Button(
-        description='⮜',
+        description="⮜",
         disabled=False,
-        tooltip='Previous',
+        tooltip="Previous",
     )
     next_button = ipywidgets.Button(
-        description='⮞',
+        description="⮞",
         disabled=False,
-        tooltip='Next',
+        tooltip="Next",
     )
 
     def show_frame(idx):
@@ -279,26 +301,26 @@ def display_detections(
         fig = plotly.express.imshow(img)
         for track, x, y, width, height in svt_s0_detections[str(idx)].values():
             fig.add_shape(
-                type='rect',
+                type="rect",
                 x0=x,
                 x1=x + width,
                 y0=y,
                 y1=y + height,
-                line_color='red',
+                line_color="red",
             )
             fig.add_annotation(
                 x=x,
                 y=y,
-                text=f'Track {track}',
-                font={'color': 'red'},
+                text=f"Track {track}",
+                font={"color": "red"},
             )
         figure_output.clear_output()
         with figure_output:
             fig.show()
 
     def on_frame_slider_change(change):
-        frame_label.value = frame_id_filename_pair[change['new']][1]
-        show_frame(change['new'])
+        frame_label.value = frame_id_filename_pair[change["new"]][1]
+        show_frame(change["new"])
 
     def on_click_previous(button):
         del button
@@ -312,7 +334,7 @@ def display_detections(
 
     previous_button.on_click(on_click_previous)
     next_button.on_click(on_click_next)
-    frame_slider.observe(on_frame_slider_change, names='value')
+    frame_slider.observe(on_frame_slider_change, names="value")
 
     frame_label = ipywidgets.Label(frame_id_filename_pair[0][1])
     show_frame(0)
@@ -384,8 +406,8 @@ def make_frames_with_tracks(
     csv_fpath: str, in_frames_dir: str, out_frames_dir: str
 ) -> None:
     track_data = pd.read_csv(csv_fpath)
-    track_data.sort_values('frame_filename', inplace=True)
-    track_data.set_index(keys=['frame_filename'], drop=False, inplace=True)
+    track_data.sort_values("frame_filename", inplace=True)
+    track_data.set_index(keys=["frame_filename"], drop=False, inplace=True)
 
     for frame_fname in sorted(os.listdir(in_frames_dir)):
         in_fpath = os.path.join(in_frames_dir, frame_fname)
